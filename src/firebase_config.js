2b01bc17427a925b0135faa1373b746a3c269309
smartDCs <39224705+smartDCs@users.noreply.github.com>
2024-04-26 10:11:46 -0500

    Update firebase_config.js

--- src/firebase_config.js
+++ src/firebase_config.js
@@ -1,10 +1,1 @@
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyCEkU4iM8abcnz-jTxOKr38VrCTuoP3Ckw",
-  authDomain: "nexhub-d92c0.firebaseapp.com",
-  projectId: "nexhub-d92c0",
-  storageBucket: "nexhub-d92c0.appspot.com",
-  messagingSenderId: "610967672597",
-  appId: "1:610967672597:web:f4b9f6fef2c3f0a6cabb1c"
-};
-
